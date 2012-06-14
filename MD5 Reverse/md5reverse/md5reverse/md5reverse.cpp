fd05c9d2f473f5f19f60e96072e42a221a2d5170
bwall <nightstrike9809@gmail.com>
2012-06-14 09:46:20 -0400

    something

--- MD5 Reverse/md5reverse/md5reverse/md5reverse.cpp
+++ MD5 Reverse/md5reverse/md5reverse/md5reverse.cpp
@@ -7,9 +7,13 @@ using namespace std;
 
 int main (int argc, char *argv[])
 {
-	string hash("e1b849f9631ffc1829b2e31402373e3c");
+	string hash("433d991c709a37eaf7e6d12f0d17b55d");
 	transform(hash.begin(), hash.end(), hash.begin(), (int(*)(int)) tolower);
-	string alpha("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+	string alpha;
+	for(int x = 0; x < 255; x++)
+	{
+		alpha.append(1, (char)x);
+	}
 	md5string cracker(hash, alpha, "", 0, 8);
 	return (0);
 }
